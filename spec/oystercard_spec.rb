344e73b71d127f490337a7330215aa2f13de77ca
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-10 18:09:04 +0100

    Test refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-  let (:amount) { 5 }
+  let(:amount) { 5 }
+  let(:station) { double :station }
   
   it 'has a balance of 0' do
     expect(subject.balance).to eq 0
@@ -34,15 +35,13 @@ describe Oystercard do
   describe '#touch_in' do
     it { is_expected.to respond_to :touch_in }
 
-    context 'had been topped up' do
+    context 'has touched in with a balance' do
       before do
         subject.top_up amount
+        subject.touch_in
       end
 
-      it 'can start a journey' do
-        subject.touch_in
-        expect(subject).to be_in_journey
-      end
+      it { is_expected.to be_in_journey }
     end
 
     context 'has not been topped up' do
@@ -56,16 +55,14 @@ describe Oystercard do
   describe '#touch_out' do
     it { is_expected.to respond_to :touch_out }
       
-    context 'has previously touched in' do
+    context 'has touched out' do
       before do
         subject.top_up amount
         subject.touch_in
         subject.touch_out
       end
 
-      it 'can end a journey' do
-        expect(subject).not_to be_in_journey
-      end
+      it { is_expected.not_to be_in_journey }
 
       it 'charges the card' do
         expect { subject.touch_out }.to change { subject.balance }.by -Oystercard::MINIMUM_FARE
