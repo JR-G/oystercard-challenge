8b62027fddb01f9dc212cbd4b0bfa3b50ca1f004
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-10 15:20:13 +0100

    Added a failing test for the top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,10 @@ describe Oystercard do
   it 'has a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'can be topped up' do
+    amount = 5
+    expect(subject.top_up(amount)).to change { subject.balance }.by(amount)
+  end
   
 end
