8c5b14b05918ddcfec1e9f340827f55848663231
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-11 15:47:41 +0100

    Added failing tests for the journey class

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,17 @@
 require 'journey'
 
 describe Journey do
+  let(:station) { double "station", zone: 1 }
   
+  it { is_expected.to respond_to :finish }
+  it { is_expected.to respond_to :fare }
+  it { is_expected.to respond_to :complete? }
+  
+  context 'given an entry station' do
+    subject { described_class.new(entry_station: station) }
+
+    it 'has an entry station' do
+      expect(subject.entry_station).to eq station
+    end
+  end
 end
