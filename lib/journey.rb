94e3fafdb96742d59fd42e9a6117f758345c882f
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-11 16:23:32 +0100

    Passed the journey class tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,19 @@
 class Journey
+  attr_reader :entry_station
+
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
+  def initialize(entry_station = nil)
+    @entry_station = entry_station
+  end
+
+  def finish
+  end
+
+  def fare
+  end
+
+  def complete?
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,17 +1,23 @@
 require 'journey'
 
 describe Journey do
-  let(:station) { double "station", zone: 1 }
-  
+  let(:station) { double :station, zone: 1}
+
   it { is_expected.to respond_to :finish }
   it { is_expected.to respond_to :fare }
   it { is_expected.to respond_to :complete? }
-  
-  context 'given an entry station' do
-    subject { described_class.new(entry_station: station) }
 
-    it 'has an entry station' do
-      expect(subject.entry_station).to eq station
+  describe '#initialized' do
+    it 'has an empty station' do
+      expect(subject.entry_station).to be_nil
+    end
+
+    context 'given an entry station' do
+      subject {described_class.new station }
+  
+      it 'has an entry station' do
+        expect(subject.entry_station).to eq station
+      end
     end
   end
 end
